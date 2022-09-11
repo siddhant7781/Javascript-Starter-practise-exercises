1f4c69f4d22885bef7b9380d8487f723008e4d9c
siddhant paudel <70979294+siddhant7781@users.noreply.github.com>
2022-09-11 22:55:13 +0545

    push

--- soln5-creating new string.js
+++ soln5-creating new string.js
@@ -1,1 +1,7 @@
-//5. Write a javascript programto create a new string adding "New!" in front of a given string. If the givem string begins with "New!" already then return the original string.
+//5. Write a javascript program to create a new string adding "New!" in front of a given string. If the givem string begins with "New!" already then return the original string.
+
+const addNew = (str) => str.indexOf('New!')===0?str: `New! ${str}`;
+
+console.log(addNew('Telephone'));
+
+console.log(addNew('New! car'));
